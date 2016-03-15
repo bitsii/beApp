b58fe6d4bdea05f9ec27b4559d62ee0865a23c2a
Craig <Craig>
2016-03-14 18:52:18 -0700

    add zipping

--- scripts/relprep.bat
+++ scripts/relprep.bat
@@ -4,3 +4,11 @@ call uglifyjs ..\apprun\App\Dz\BEL_4_Base.js > ..\apprun\App\Dz\BEL_4_Base.js.1
 del /q ..\apprun\App\Dz\BEL_4_Base.js
 
 move ..\apprun\App\Dz\BEL_4_Base.js.1 ..\apprun\App\Dz\BEL_4_Base.js
+
+cd ..\apprun\App\Dz
+
+del ..\..\..\Dz.zip
+
+zip ..\..\..\Dz.zip *
+
+cd ..\..\..\app
