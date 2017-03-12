909f953da3d5ecd9191ea5f792214a57ebb941ec
Craig <Craig>
2017-03-12 13:12:08 -0700

    fix release to not include 3rd party jars

--- scripts/iubrel.sh
+++ scripts/iubrel.sh
@@ -8,6 +8,14 @@ mv ../apprun/App/IUHub/IUHub_BEL_4_Base.js.1 ../apprun/App/IUHub/IUHub_BEL_4_Bas
 
 cd ../apprun/App/IUHub
 
+mv BEL_4_Base_lib_jv.jar BEL_4_Base_lib_jv.ja
+mv BEL_4_Base_lui_jv.jar BEL_4_Base_lui_jv.ja
+
+rm *.jar
+
+mv BEL_4_Base_lib_jv.ja BEL_4_Base_lib_jv.jar
+mv BEL_4_Base_lui_jv.ja BEL_4_Base_lui_jv.jar
+
 rm ../../../IUBHub.zip
 
 zip ../IUBHub.zip ./*
