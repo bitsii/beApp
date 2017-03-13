eb8a6dce080c9406b859da4183a12085e8e3c6b8
Craig <Craig>
2017-03-12 21:00:34 -0700

    android hub still need asset move

--- scripts/iuh5jvad.sh
+++ scripts/iuh5jvad.sh
@@ -23,51 +23,17 @@ cp ../abe-pl/system/jv/be/*java android/IUHub/app/src/main/java/be
 
 cp extlibs/IUHubAd/* android/IUHub/app/libs/
 
+lae=$?;if [[ $lae -ne 0 ]]; then exit $lae; fi
+
+java be.BEL_4_Base ../abe-pl/source/base/Uses.be --buildFile build/base.txt --deployPath ../apprun/App/IUHub/d --buildPath ../apprun/App/IUHub --emitLang js --outputPlatform linux --ownProcess false -mainClass=IUHub:Eui ../abe-pl/source/extended/Log.be source/IUHubBr.be source/BrowserEUI.be
+
+lae=$?;if [[ $lae -ne 0 ]]; then exit $lae; fi
+
 exit 0
-
-javac ../abe-pl/system/jv/be/*.java ../apprun/App/IUHub/Base/target/jv/be/*.java
-
-lae=$?;if [[ $lae -ne 0 ]]; then exit $lae; fi
-
-java be.BEL_4_Base ../abe-pl/source/base/Uses.be --buildFile build/base.txt --deployPath ../apprun/App/IUHub/d --buildPath ../apprun/App/IUHub --emitLang js --ownProcess false -mainClass=IUHub:Eui ../abe-pl/source/extended/Log.be source/IUHubBr.be source/BrowserEUI.be
-
-lae=$?;if [[ $lae -ne 0 ]]; then exit $lae; fi
-
-cd ../apprun/App/IUHub/Base/target/jv
-jar -cf ../../../BEL_4_Base_lui_jv.jar .
-cd ../../../../../../ioturl
-
-cd ../abe-pl/system/jv
-jar -cf ../../../apprun/App/IUHub/BEL_4_Base_lib_jv.jar .
-cd ../../../ioturl
-
-find ../abe-pl/system -name "*.class" -exec rm {} \;
 
 #hub
 cp ../apprun/App/IUHub/Base/target/js/be/BEL_4_Base.js ../apprun/App/IUHub/IUHub_BEL_4_Base.js
-cp scripts/upgrade.bat ../apprun/App/IUHub
-cp scripts/postupgrade.bat ../apprun/App/IUHub
-cp scripts/upgrade.sh ../apprun/App/IUHub
-cp scripts/upgrade2.sh ../apprun/App/IUHub
-cp scripts/postupgrade.sh ../apprun/App/IUHub
-cp scripts/startiuh.sh ../apprun/App/IUHub
-cp scripts/iuhrun.sh ../apprun/App/IUHub
-cp scripts/iuhcmdrs.sh ../apprun/App/IUHub
-cp scripts/iuhcmd.sh ../apprun/App/IUHub
-cp scripts/upgrade.bat ../apprun/App/IUHub
 cp source/IU.html ../apprun/App/IUHub
-cp extlibs/IUHubAd/* ../apprun/App/IUHub
 cp icons/* ../apprun/App/IUHub
 cp LICENSE.txt ../apprun/App/IUHub
 cp LICENSE-MPL.txt ../apprun/App/IUHub
-
-#cam
-cp scripts/uppic.bat ../apprun/App/IUHub
-cp scripts/uppic.sh ../apprun/App/IUHub
-cp scripts/getcams.bat ../apprun/App/IUHub
-cp scripts/getcams.sh ../apprun/App/IUHub
-cp scripts/motionrun.sh ../apprun/App/IUHub
-cp scripts/camclean.sh ../apprun/App/IUHub
-cp source/MOCAM.conf ../apprun/App/IUHub
-
-./scripts/iub5jvrun.sh $*
