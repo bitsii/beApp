ffa4928c7d369ba254054b99b36b6ed3b0693f21
craig <craig@here>
2018-06-09 23:07:21 -0700

    gen cert if none present

--- scripts/starthap.sh
+++ scripts/starthap.sh
@@ -2,5 +2,13 @@
 
 mkdir -p Data/KBridge/haproxy
 
-haproxy -D -p Data/KBridge/haproxy/haproxy.pid -f Data/KBridge/haproxy/haproxy.cfg -sf $(cat Data/KBridge/haproxy/haproxy.pid)
+if [ ! -e "./Data/KBridge/haproxy/cert.pem" ]; then
+   openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365 -nodes -subj "/C=US/ST=Oregon/L=Portland/O=Company Name/OU=Org/CN=www.example.com"
+   cat cert.pem key.pem > all.pem
+   rm cert.pem
+   rm key.pem
+   mv all.pem Data/KBridge/haproxy/cert.pem
+fi
+
+haproxy -dV -D -p Data/KBridge/haproxy/haproxy.pid -f Data/KBridge/haproxy/haproxy.cfg -sf $(cat Data/KBridge/haproxy/haproxy.pid)
 
