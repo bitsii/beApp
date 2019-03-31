89e208d6bcc3d62ccac7aead439f0ddda40eac54
Bitsii <Bitsii>
2019-03-30 23:31:18 -0700

    ios cc builds

---
+++ scripts/bldbaccis.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+export APPBLDNM=${PWD##*/}
+
+mkdir -p ../apprun
+mkdir -p ../apprun/App
+mkdir -p ../apprun/Data
+mkdir -p ../apprun/Data/$APPBLDNM
+
+rm -rf ../apprun/App/$APPBLDNM
+mkdir -p ../apprun/App/$APPBLDNM
+
+una=`uname -a`
+
+mono --debug ../abelii/target5/BEX_E_mcs.exe ../abelii/source/base/Uses.be --buildFile ../abeliiApp/build/shared.txt --deployPath ../apprun/App/$APPBLDNM/d --buildPath ../apprun/App/$APPBLDNM --emitLang cc --singleCC true --saveIds false --emitFlag ccSgc --emitFlag ccBeMain -mainClass=App:AppStart --buildFile build/build.txt $BEBLDARGS ../abelii/source/extended/Log.be ../abeliiApp/source/App.be ../abeliiApp/source/BrowserUI.be ../abeliiApp/source/BrowserJvAd.be ../abeliiApp/source/Db.be ../abeliiApp/source/SlDbJv.be   
+
+lae=$?;if [[ $lae -ne 0 ]]; then exit $lae; fi
+
+mkdir -p ios/$APPBLDNM/$APPBLDNM
+mkdir -p ios/resources/App/BNote
+rm -f ios/$APPBLDNM/$APPBLDNM/BE*.hpp
+rm -f ios/$APPBLDNM/$APPBLDNM/BE*.cpp
+cp ../apprun/App/$APPBLDNM/Base/target/cc/be/BE*.hpp ios/$APPBLDNM/$APPBLDNM
+cp ../apprun/App/$APPBLDNM/Base/target/cc/be/BE*.cpp ios/$APPBLDNM/$APPBLDNM
+
+lae=$?;if [[ $lae -ne 0 ]]; then exit $lae; fi
+
+mono --debug ../abelii/target5/BEX_E_mcs.exe ../abelii/source/base/Uses.be --buildFile ../abeliiApp/build/base.txt --deployPath ../apprun/App/$APPBLDNM/d --buildPath ../apprun/App/$APPBLDNM --emitLang js --ownProcess false --buildFile build/buildbr.txt ../abelii/source/extended/Log.be ../abeliiApp/source/BrowserEUI.be
+
+lae=$?;if [[ $lae -ne 0 ]]; then exit $lae; fi
+
+cp ../apprun/App/$APPBLDNM/Base/target/js/be/BEX_E.js ios/resources/App/BNote/BEX_E.js
+
+cp -R resources/* ios/resources/App/BNote
+
+cd ../apprun/App/$APPBLDNM
