8f56e57b65d60ae1114aa3fd0a08622ca4d4c37e
Bitsii <Bitsii>
2019-12-14 16:51:43 -0800

    stopwajvrs.sh

---
+++ scripts/stopwajvrs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+cd "${0%/*}"
+
+export APPBLDNM=${PWD##*/}
+
+cd ../..
+
+export APPRUNDIR=${PWD}
+#echo $APPRUNDIR
+
+export OSTYPE=`uname`
+
+if [ "$OSTYPE" == "Darwin" ]; then
+
+  export PATH=$PATH:/usr/sbin:/usr/local/bin
+
+fi
+
+if [[ $OSTYPE == *"MINGW"* ]]; then
+  #echo "Is Mingw"
+  export OSTYPE="Mingw"
+fi
+
+if [ "$OSTYPE" == "Darwin" ]; then
+  lsof -d cwd | grep $APPRUNDIR | grep java | awk '{$1=$1}1' | cut -d " " -f 2 | xargs -I{} ps -fe {} | grep $APPBLDNM | awk '{$1=$1}1' | cut -d " " -f 3 | xargs -I{} kill {}
+  lsof -d cwd | grep $APPRUNDIR | grep java | awk '{$1=$1}1' | cut -d " " -f 2 | xargs -I{} ps -fe {} | grep $APPBLDNM | awk '{$1=$1}1' | cut -d " " -f 2 | xargs -I{} kill {}
+fi
