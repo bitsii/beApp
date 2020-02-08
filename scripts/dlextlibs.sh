b93796613b3c7af9b5d605b33b7386e0775ead78
Craig <craig@here>
2020-02-08 09:03:24 -0800

    rm sqlite

--- scripts/dlextlibs.sh
+++ scripts/dlextlibs.sh
@@ -7,16 +7,3 @@ curl -L https://repo1.maven.org/maven2/javax/servlet/javax.servlet-api/3.1.0/jav
 
 curl -L https://repo1.maven.org/maven2/org/eclipse/jetty/aggregate/jetty-all/9.4.0.M1/jetty-all-9.4.0.M1-uber.jar > jetty-all-9.4.0.M1-uber.jar
 
-curl -L https://bitbucket.org/xerial/sqlite-jdbc/downloads/sqlite-jdbc-3.19.3.jar > sqlite-jdbc-3.19.3.jar
-
-mkdir ../ba
-cp sqlite-jdbc-3.19.3.jar ../ba
-
-mkdir ../ca
-cp sqlite-jdbc-3.19.3.jar ../ca
-
-mkdir ../ba
-cp sqlite-jdbc-3.19.3.jar ../ba
-
-mkdir ../ca
-cp sqlite-jdbc-3.19.3.jar ../ca
