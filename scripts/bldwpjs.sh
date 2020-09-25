0fb013f8a8387aedf7fb4bf5c29cc61fe0aadcb8
craig <craig@here>
2020-09-24 20:11:59 -0700

    web page js only support

---
+++ scripts/bldwpjs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export APPBLDNM=${PWD##*/}
+
+mkdir -p ../apprun
+mkdir -p ../apprun/App
+mkdir -p ../apprun/App/$APPBLDNM
+
+mono --debug ../brace/target5/BEX_E_mcs.exe ../brace/source/base/Uses.be --buildFile ../braceApp/build/base.txt $BRBLDARGS --deployPath ../apprun/App/$APPBLDNM/d --buildPath ../apprun/App/$APPBLDNM --emitLang js --ownProcess false --buildFile build/buildbr.txt ../brace/source/extended/Log.be ../braceApp/source/BrowserEUI.be
+
+lae=$?;if [[ $lae -ne 0 ]]; then exit $lae; fi
+
+cp ../apprun/App/$APPBLDNM/Base/target/js/be/BEX_E.js ../apprun/App/$APPBLDNM/BEX_E.js
+
+if [ ! -z "$BEPOSTBUILD" -a "$BEPOSTBUILD" != " " ]; then
+  eval "$BEPOSTBUILD"
+fi
